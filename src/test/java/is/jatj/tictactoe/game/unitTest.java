e1642f4c922f218c691fbdb2725d233678be7ed4
Jónas Arnþór Guðmundsson <Jonasag@clients-pool-1.nat.ru.is>
2015-10-27 15:45:15 +0000

    lala

--- src/test/java/is/jatj/tictactoe/game/unitTest.java
+++ src/test/java/is/jatj/tictactoe/game/unitTest.java
@@ -7,8 +7,6 @@ import main.java.is.jatj.tictactoe.game.TicTacToe;
 
 public class unitTest{
 	// usesless comment
-	public class unitTest{
-	// usesless comment
 	public static void main(String args[]) {
 	      org.junit.runner.JUnitCore.main("src.TicTacToe.unitTest");
 	    }
@@ -40,110 +38,14 @@ public class unitTest{
 		}
 		
 		@Test
-		public void testWinnerH() {
+		public void testTwoNumbers() {
 			game.makeMove(0, 0);
 			game.makeMove(1, 0);
 			game.makeMove(2, 0);
 			assertEquals(true, game.checkForWinner());
 			
 		}	
+
 		
-		@Test
-		public void testWinnerV() {
-			game.makeMove(0, 0);
-			game.makeMove(3, 0);
-			game.makeMove(6, 0);
-			assertEquals(true, game.checkForWinner());
-			
-		}
-		@Test
-		public void testWinnerA() {
-			game.makeMove(0, 0);
-			game.makeMove(4, 0);
-			game.makeMove(8, 0);
-			assertEquals(true, game.checkForWinner());
-			
-		}
-		
-		@Test
-		public void testplayerO() {
-			game.makeMove(0, 0);
-			assertEquals('O', game.board[0][0]);
-		}	
-		@Test
-		public void testplayerX() {
-			game.makeMove(0, 1);
-			assertEquals('X', game.board[0][0]);
-		}
-		@Test
-		public void testcleanboard() {
-			game.makeMove(0, 1);
-			game.makeMove(3, 1);
-			game.makeMove(4, 1);
-			game.makeMove(2, 1);
-			game.makeMove(8, 1);
-			game.makeMove(6, 1);
-			game.makeMove(7, 1);
-			game.makeMove(1, 1);
-			game.makeMove(5, 1);
-			game.cleanBoard();
-			char c[] = new char[9];
-			c[0] = '1';
-			c[1] = '2';
-			c[2] = '3';
-			c[3] = '4';
-			c[4] = '5';
-			c[5] = '6';
-			c[6] = '7';
-			c[7] = '8';
-			c[8] = '9';
-			
-			int co = 0;
-			for(int i = 0; i < 3; i++)
-			{
-				
-				for(int j = 0; j < 3; j++)
-				{
-					
-					assertEquals(c[co], game.board[i][j]);
-					co++;
-				}
-			}
-			
-		}
-		
-		@Test
-		public void testintializeBoard() {
-			game.initializeBoard();
-			char c[] = new char[9];
-			c[0] = '1';
-			c[1] = '2';
-			c[2] = '3';
-			c[3] = '4';
-			c[4] = '5';
-			c[5] = '6';
-			c[6] = '7';
-			c[7] = '8';
-			c[8] = '9';
-			
-			int co = 0;
-			for(int i = 0; i < 3; i++)
-			{
-				
-				for(int j = 0; j < 3; j++)
-				{
-					
-					assertEquals(c[co], game.board[i][j]);
-					co++;
-				}
-			}	
-		}	
-		
-		@Test
-		public void testinitializePlayer() {
-			game.initializePlayer();
-			assertEquals('O', game.player[0]);
-			assertEquals('X', game.player[1]);
-		}
-}
+	
 }

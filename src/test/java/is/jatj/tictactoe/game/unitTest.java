688a544e8e2f1b0630c1314bcda0c3e16fd2af64
Jónas Arnþór Guðmundsson <Jonasag@clients-pool-1.nat.ru.is>
2015-10-27 16:35:17 +0000

    unit test

--- src/test/java/is/jatj/tictactoe/game/unitTest.java
+++ src/test/java/is/jatj/tictactoe/game/unitTest.java
@@ -72,6 +72,42 @@ public class unitTest{
 			game.makeMove(0, 1);
 			assertEquals('X', game.board[0][0]);
 		}
+		@Test
+		public void testcleanboard() {
+			game.makeMove(0, 1);
+			game.makeMove(3, 1);
+			game.makeMove(4, 1);
+			game.makeMove(2, 1);
+			game.makeMove(8, 1);
+			game.makeMove(6, 1);
+			game.makeMove(7, 1);
+			game.makeMove(1, 1);
+			game.makeMove(5, 1);
+			game.cleanBoard();
+			char c[] = new char[9];
+			c[0] = '1';
+			c[1] = '2';
+			c[2] = '3';
+			c[3] = '4';
+			c[4] = '5';
+			c[5] = '6';
+			c[6] = '7';
+			c[7] = '8';
+			c[8] = '9';
+			
+			int co = 0;
+			for(int i = 0; i < 3; i++)
+			{
+				
+				for(int j = 0; j < 3; j++)
+				{
+					
+					assertEquals(c[co], game.board[i][j]);
+					co++;
+				}
+			}
+			
+		}
 
 		
 	
